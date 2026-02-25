918d32f5e7fa7f81bc2bdc74233be7c58fd96d0b
bielwolf <gabryellobo@gmail.com>
2026-02-25 10:00:06 -0300

    feat: integrar TypeORM com SQLite para gerenciamento de pets
    - Adicionadas as dependências do TypeORM e SQLite no package.json.
    - Inicializada a fonte de dados (Data Source) do TypeORM no app.ts.
    - Criada a configuração do dataSource para o banco SQLite.
    - Implementado o modelo PetEntity para a estrutura de dados do pet.
    - Desenvolvido o PetRepository para operações no banco.
    - Atualizado o PetController para utilizar o PetRepository nas operações CRUD.
    - Modificado o petRouter para instanciar o PetController com o PetRepository.
    - Aprimorada a configuração do TypeScript para suportar decorators e metadata.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express, { Response } from "express";
 import router from "./routes";
+import "reflect-metadata";
+import { AppDataSource } from "./config/dataSource";
 
 const app = express();
 app.use(express.json());
 router(app);
+
+AppDataSource.initialize().then(() => {
+    console.log("Data Source has been initialized!");
+}).catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+});
 
 export default app;
 
--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import type TipoPet from "../tipos/TipoPet";
 import EnumEspecie from "../enum/EnumEspecie";
+import PetRepository from "../repositories/PetRepository";
+import PetEntity from "../entities/PetEntity";
 
 let listaDePets: TipoPet[] = [];
 
@@ -11,29 +13,32 @@ function geraId() {
 }
 
 export default class PetController {
+    constructor(private repository: PetRepository) {}
+
     criaPet(req:Request, res:Response) {
-        const { nome, especie, dataDeNascimento, adotado } = <TipoPet>req.body;
-        if(Object.values(EnumEspecie).includes(especie)) {
+        const { nome, especie, dataDeNascimento, adotado } = <PetEntity>req.body;
+        if(!Object.values(EnumEspecie).includes(especie)) {
             return res.status(400).json({ message: "Espécie inválida" });
         }
-        const novoPet: TipoPet = {
-            id: geraId(),
-            nome,
-            especie,
-            dataDeNascimento,
-            adotado
-        }
-        listaDePets.push(novoPet);
+        const novoPet = new PetEntity();
+            novoPet.id = geraId();
+            novoPet.nome = nome;
+            novoPet.especie = especie;
+            novoPet.dataDeNascimento = dataDeNascimento;
+            novoPet.adotado = adotado;
+
+        this.repository.criaPet(novoPet);
         return res.status(201).json(novoPet);
     }
 
-    listarPets(req:Request, res:Response) {
+    async listarPets(req:Request, res:Response) {
+        const listaDePets = await this.repository.listarPets();
         return res.status(200).json(listaDePets);
     }
 
     atualizaPet(req:Request, res:Response) {
         const { id } = req.params;
-        const { nome, especie, dataDeNascimento, adotado } = <TipoPet>req.body;
+        const { nome, especie, dataDeNascimento, adotado } = <PetEntity>req.body;
         const pet = listaDePets.find((p) => p.id === Number(id));
         if (!pet) {
             return res.status(404).json({ message: "Pet não encontrado" });
